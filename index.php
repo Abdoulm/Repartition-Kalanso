534e2b8cb5366af235a5c525d6fa8406cc1ee322
Abdoul Maiga <maiga.abdoul@odc.local>
2021-09-08 19:41:43 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+ 		 <!-- Bootstrap CSS -->
+
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!-- Bootstrap jS -->
+  		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+  		<link rel="stylesheet" type="text/css" href="style.css">
+		<title></title>	
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	    	<a class="navbar-brand" href="#">
+	    	<img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+	   	 Bootstrap
+	  		</a>
+
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	      <div class="mr-auto"></div>
+	      <ul class="navbar-nav my-2 my-lg-0">
+		<li class="nav-item active">
+	    	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	    </li>
+	
+
+	        <div class="dropdown">
+			  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			    Partition Groupe
+			  </button>
+			  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			    <a class="dropdown-item" href="#">Groupe 1</a>
+			    <a class="dropdown-item" href="#">Groupe 2</a>
+			    <a class="dropdown-item" href="#">Groupe 3</a>
+			    <a class="dropdown-item" href="#">Groupe 4</a>
+			    <a class="dropdown-item" href="#">Groupe 5</a>
+ 			  </div>
+			</div>
+
+	      </ul>
+
+	      </div>
+  		</nav>
+
+	
+	</body>
+	</html>               
